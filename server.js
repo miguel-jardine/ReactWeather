f50b6d8303bf561e302933bbeac22e6b642c1c65
Miguel Jardine <miguel.jardine@gmail.com>
2016-09-18 05:59:57 -0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var 
     // Config
     APPLICATION = "ReactWeather",
-    PORT = 4000,
 
     // Base
     express         = require("express"),
@@ -9,9 +8,17 @@ var
     app             = express(),
 end_var_def = [];
 
+const PORT = process.env.PORT || 4000;
 
 app
     .use(express.static('public'))
+
+    .use((req, res, next) => {
+        if (req.headers["x-forward-proto"] === "http")
+            next();
+        else   
+            res.redirect("http://" + req.hostname + req.url);
+    })
 
     // START SERVER.
     .listen(PORT, function () {
